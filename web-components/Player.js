1341f46795fb73354c60046fa84dcc6dac8d40f4
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-04-09 12:40:18 -0400

    Changed spacing

--- web-components/Player.js
+++ web-components/Player.js
@@ -19,7 +19,7 @@ const Player = ({
       }
       .cards {
         display: grid;
-        grid-template-columns: repeat(3, 1fr);
+        grid-template-columns: repeat(3, max-content);
         width: auto;
         gap: 1rem;
       }
