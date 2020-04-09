c338481677b1821ae7e87378a199a1545d2d5e12
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-04-09 13:08:14 -0400

    Removed duplicate dependency

--- dependencies.js
+++ dependencies.js
@@ -1,10 +1,10 @@
-import { html } from "https://unpkg.com/lit-html@1.2.1/lit-html.js";
 import {
   component,
   useState,
   useMemo,
   useEffect,
   useCallback,
+  html,
 } from "https://unpkg.com/haunted@4.7.0/haunted.js";
 
 export { html, component, useState, useMemo, useEffect, useCallback };
