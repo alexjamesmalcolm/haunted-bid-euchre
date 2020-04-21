ce23913f5919d42513340794a68044462cdf10f5
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-04-20 20:33:41 -0400

    When name is null return empty string

--- store.js
+++ store.js
@@ -1,6 +1,7 @@
 export class Store {
   static get name() {
-    return localStorage.getItem("name");
+    const name = localStorage.getItem("name");
+    return name ? name : "";
   }
   static setName(name) {
     localStorage.setItem("name", name);
